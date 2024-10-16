a11ac669d9af8592f42d9379dcae9db44ed97597
baovu <vubao.edu@gmail.com>
2024-10-16 20:01:37 +0700

    init

---
+++ b01/bai03.py
@@ -0,0 +1,4 @@
+# Hãy viết chương trình khai báo một biến dùng để lưu năm sinh của một người. Hãy cho biết người ấy bao nhiêu tuổi.
+nam_sinh = 2016
+tuoi = 2024 - nam_sinh
+print(tuoi) # In giá trị của biến ra màn hình
