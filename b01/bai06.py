ea85ef5d546a8aa47461fd9480417621ac08644b
baovu <vubao.edu@gmail.com>
2024-10-16 21:41:17 +0700

    init

---
+++ b01/bai06.py
@@ -0,0 +1,12 @@
+# Hãy viết chương trình cho phép nhập vào 2 số. 
+# Tính tổng của hai số ấy.
+
+# Nhập (input)
+a = int(input('Nhap so thu nhat:')) # Trả về giá trị kiểu string
+b = int(input('Nhap so thu hai:')) # Ép kiểu
+
+# Xử lý (process)
+c = a + b
+
+# Xuất (output)
+print('Tong:', c)
